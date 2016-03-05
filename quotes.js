1d0cc30c1d96eed41194a16d3f5f4548d3dec9c2
dbertella <daniele.bertella@gmail.com>
2016-03-05 12:44:46 +0100

    add jpg smaller size, rewrite js function to refresh quotes without refreshing the pages

--- quotes.js
+++ quotes.js
@@ -1,40 +1,35 @@
-// ==============================================
-// Copyright 2004 by CodeLifter.com
-// Free for all; but please leave in this header.
-// ==============================================
-
 var Quotation = [
-'Interessante',
-'Molto interessante',
-'Più induttivo',
-'Il pesce puzza sempre dalla testa',
-'Per cambiare VERAMENTE',
-'Una soluzione end-to-end',
-'Se fossero bravi li avrebbero già venduti',
-'Che violenza..!',
-'Ah sono arrivati altri CV',
-'Concordo',
-'Io sono un boy scout',
-'Venite in piscina con me a fare apnea?',
-'Devo andare a mettere a letto le bambine',
-'Facciamo un prototipo e vendiamolo',
-'Ho una proposta',
-'Siamo una start up, siamo incubati in Reply',
-'Quelli sono soldi facili',
-'Facciamo i mini-me',
-'Per imparare a vendere bisognare fare almeno una figura di merda al giorno',
-'Mi piace',
-'Non siamo una boutique di design',
-'Facciamo un workshop di design thinking',
-'Sono super competitivo io',
-'Mettiamo lì uno dal cliente per 6 mesi',
-'Usiamo le logiche di una start up',
-'Le bambine non mi hanno fatto dormire',
-'Non siamo mica Accenture',
-'Metti giù una presentazione a bullet point',
-'Sulla tecnologia non mi fregano',
-'non vedo problemi, vedo opportunità',
-'se c\'è mediocrità ci sono più opportunità',
+	'Interessante',
+	'Molto interessante',
+	'Più induttivo',
+	'Il pesce puzza sempre dalla testa',
+	'Per cambiare VERAMENTE',
+	'Una soluzione end-to-end',
+	'Se fossero bravi li avrebbero già venduti',
+	'Che violenza..!',
+	'Ah sono arrivati altri CV',
+	'Concordo',
+	'Io sono un boy scout',
+	'Venite in piscina con me a fare apnea?',
+	'Devo andare a mettere a letto le bambine',
+	'Facciamo un prototipo e vendiamolo',
+	'Ho una proposta',
+	'Siamo una start up, siamo incubati in Reply',
+	'Quelli sono soldi facili',
+	'Facciamo i mini-me',
+	'Per imparare a vendere bisognare fare almeno una figura di merda al giorno',
+	'Mi piace',
+	'Non siamo una boutique di design',
+	'Facciamo un workshop di design thinking',
+	'Sono super competitivo io',
+	'Mettiamo lì uno dal cliente per 6 mesi',
+	'Usiamo le logiche di una start up',
+	'Le bambine non mi hanno fatto dormire',
+	'Non siamo mica Accenture',
+	'Metti giù una presentazione a bullet point',
+	'Sulla tecnologia non mi fregano',
+	'non vedo problemi, vedo opportunità',
+	'se c\'è mediocrità ci sono più opportunità',
 ];
 
 
@@ -42,8 +37,20 @@ var Quotation = [
 // Do not change anything below this line
 // ======================================
 var Q = Quotation.length;
-var whichQuotation=Math.round(Math.random()*(Q-1));
-function showQuotation() {
+var dani = document.querySelector('.dani');
+
+var showQuotation = function() {
+	var whichQuotation = Math.round(Math.random() * (Q - 1));
+	var wichImg = Math.ceil(Math.random() * 5);
+	var img = document.createElement('img');
+	img.src = 'danisales-0' + wichImg + '.jpg';
+	dani.innerHTML = '';
+	dani.appendChild(img);
 	document.querySelector('.quote').innerText = Quotation[whichQuotation];
 }
+
+dani.addEventListener('click', showQuotation, false);
+
 showQuotation();
+
+
